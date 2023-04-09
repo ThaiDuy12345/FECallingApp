e6fa9915276e4f628574f73db2250b807c508cb0
ThaiDuy12345 <nguyenngocthaiduy.nt@gmail.com>
2023-04-10 02:23:44 +0700

    ádasdasd

--- src/components/ChatDMRender.jsx
+++ src/components/ChatDMRender.jsx
@@ -24,7 +24,7 @@ export default function ChatDMRender({Objects}){
         element.current.scrollTop = element.current.scrollHeight;
     },[allMessages])
     useEffect(() => {
-        socket.current = io("https://siri-real-time-chat-server-side.vercel.app/",{
+        socket.current = io("https://siri-real-time-chat-server-side.vercel.app/socket.io",{
             withCredentials: true,
             transports: ['websocket', 'polling', 'flashsocket']
         })
--- src/components/ChatGroupRender.jsx
+++ src/components/ChatGroupRender.jsx
@@ -25,7 +25,7 @@ export default function ChatGroupRender({Objects, allGroups, setAllGroups}){
         element.current.scrollTop = element.current.scrollHeight
     })
     useEffect(() => {
-        socket.current = io("https://siri-real-time-chat-server-side.vercel.app",{
+        socket.current = io("https://siri-real-time-chat-server-side.vercel.app/socket.io",{
             withCredentials: true,
             transports: ['websocket', 'polling', 'flashsocket']
         })
