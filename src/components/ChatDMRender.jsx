534c856662a9107451e5cf8e39095c71e0570528
ThaiDuy12345 <nguyenngocthaiduy.nt@gmail.com>
2023-04-10 00:53:51 +0700

    ádasdasdasd

--- src/components/ChatDMRender.jsx
+++ src/components/ChatDMRender.jsx
@@ -24,7 +24,7 @@ export default function ChatDMRender({Objects}){
         element.current.scrollTop = element.current.scrollHeight;
     },[allMessages])
     useEffect(() => {
-        socket.current = io("https://sirichattingapp-serverside.netlify.app/.netlify/functions/api",{
+        socket.current = io("https://sirichattingapp-serverside.netlify.app/.netlify/functions/api/socket.io/",{
             transports: ['websocket'],
             withCredentials: true
         })
